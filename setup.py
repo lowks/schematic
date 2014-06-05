839cd3223fbdc2742770c5e057b19d7189390de0
Low Kian Seong <kianseong@gmail.com>
2014-06-05 11:34:48 +0800

    Update setup.py
    Adding some meat to setup.py so project is more descriptive in pypi

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup
 setup(
     name='schematic',
     version='0.2',
+    description='The worst schema versioning system, ever?',
+    url='https://github.com/jbalogh/schematic',
     scripts=['schematic'],
 )
